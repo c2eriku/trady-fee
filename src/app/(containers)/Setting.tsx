7a290692d964406fb85a9d6bb06c2a9612a2e1d8
c2erichsu <c2erichsu@gmail.com>
2025-02-02 23:36:23 +0800

    Refactor Setting component layout and styling; update description in metadata and adjust button positioning in Home component

--- src/app/(containers)/Setting.tsx
+++ src/app/(containers)/Setting.tsx
@@ -7,34 +7,41 @@ import ToggleSwitch from "../(components)/ToggleSwitch";
 export default function Setting() {
     const setting = useContext(SettingContext);
 
-    return <div className="relative mb-12 p-2 grid grid-cols-2 gap-4 border rounded w-full bg-black bg-opacity-50 z-50">
+    return <div className="relative p-2 pt-10 border border-primary rounded w-full bg-black z-40">
+        <div className="mb-4 text-center font-bold text-xl">偏好設定</div>
+        <div className=" grid grid-cols-2 gap-4">
 
-        <div className="flex items-center">券商</div>
-        <IconicSelect
-            options={brokeragesIconicSelectOptions}
-            defaultValue={setting.state.brokerage.id}
-            onChange={(value) => {
-                const updBrokerage = brokerageMap.get(value!.toString());
-                setting.dispatch({ type: "SET_BROKERAGE", payload: updBrokerage })
-            }}>
-        </IconicSelect>
+            <div className="flex items-center">券商</div>
+            <IconicSelect
+                options={brokeragesIconicSelectOptions}
+                defaultValue={setting.state.brokerage.id}
+                onChange={(value) => {
+                    const updBrokerage = brokerageMap.get(value!.toString());
+                    setting.dispatch({ type: "SET_BROKERAGE", payload: updBrokerage })
+                }}>
+            </IconicSelect>
 
-        <div className="flex items-center">賣價同步</div>
-        <ToggleSwitch value={setting.state.syncSellPrice}
-            onChange={(value) => {
-                console.log(value)
-                setting.dispatch({ type: "SET_SYNC_SELL_PRICE", payload: value })
-            }}></ToggleSwitch>
+            <div className="flex items-center">手續費折數</div>
+            <input value={setting.state.brokerage.feeDiscountRate} disabled
+                className="py-1 rounded text-foreground 
+                disabled:p-0 disabled:bg-black"/>
 
-        <div className="col-span-2 flex justify-end gap-2">
-            <button
-                onClick={() => { localStorage.removeItem('preference'); }}
-                className="rounded bg-transparent text-sm underline">刪除偏好設定</button>
-            <button
-                onClick={() => { setting.setLocalstorage(setting.state) }}
-                className="rounded bg-transparent text-sm underline">儲存偏好設定並在下次使用</button>
+
+            <div className="flex items-center">賣價同步買價</div>
+            <ToggleSwitch value={setting.state.syncSellPrice}
+                onChange={(value) => {
+                    console.log(value)
+                    setting.dispatch({ type: "SET_SYNC_SELL_PRICE", payload: value })
+                }}></ToggleSwitch>
+
+            <div className="col-span-2 flex justify-end gap-2">
+                <button
+                    onClick={() => { localStorage.removeItem('preference'); }}
+                    className="rounded bg-transparent text-sm underline">刪除偏好設定</button>
+                <button
+                    onClick={() => { setting.setLocalstorage(setting.state) }}
+                    className="rounded bg-transparent text-sm underline">儲存偏好設定並在下次使用</button>
+            </div>
         </div>
-
-
     </div>;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,22 +8,27 @@ import { useState } from "react";
 export default function Home() {
   const [isSettingOpen, setIsSettingOpen] = useState(false);
 
+  function handleSettingClick(){
+    setIsSettingOpen(!isSettingOpen);
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+
   return (
     <SettingProvider>
       {/* 設定按鈕 */}
       <button
         onClick={() => setIsSettingOpen(!isSettingOpen)}
-        className="fixed top-4 right-4 outline outline-1 text-white font-bold p-1 rounded-xl z-50
+        className="fixed bottom-4 right-4 text-white font-bold p-1 rounded-xl z-50
         hover:bg-primary-600 active:bg-primary-600">
         <Cog6ToothIcon className={`${isSettingOpen ? '-rotate-[210deg]' : 'rotate-0'} 
           size-8 transition-all`} />
       </button>
 
-      <div className="h-screen flex flex-col gap-2 items-center min-h-screen px-4 py-2 pt-20 transition-all">
+      <div className="h-screen flex flex-col gap-2 items-center min-h-screen px-4 py-2 pt-10 transition-all">
 
         {/* 控制面板 */}
         <div className={`${isSettingOpen ? "max-h-full opacity-100" : "max-h-0 opacity-0"} 
-          w-full transition-all duration-700`}>
+             w-full transform -translate-y-[2.5rem] transition-all duration-700`}>
           <Setting />
         </div>
 
