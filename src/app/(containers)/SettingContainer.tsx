dfa45f0e6290a05d85a26246a94420d00207043d
c2erichsu <c2erichsu@gmail.com>
2025-02-04 03:00:48 +0800

    Refactor Setting component; replace with SettingContainer for improved structure and functionality

---
+++ src/app/(containers)/SettingContainer.tsx
@@ -0,0 +1,81 @@
+import { Dispatch, SetStateAction, useContext, useEffect, useRef } from "react";
+import { IconicSelect } from "../(components)/IconicSelect";
+import { brokerageMap, brokeragesIconicSelectOptions } from "../(models)/brokeragesList";
+import { SettingContext } from "../(states)/SettingState";
+import ToggleSwitch from "../(components)/ToggleSwitch";
+
+interface SettingContainerProps {
+    isSettingContainerOpen: boolean,
+    setIsSettingContainerOpen: Dispatch<SetStateAction<boolean>>
+}
+
+export default function SettingContainer({ isSettingContainerOpen, setIsSettingContainerOpen }: SettingContainerProps) {
+    const containerRef = useRef<HTMLDivElement>(null);
+    const setting = useContext(SettingContext);
+
+    useEffect(() => {
+        if (!isSettingContainerOpen) return;
+
+        const handleClickOutside = (event: MouseEvent) => {
+            if (containerRef.current && !containerRef.current.contains(event.target as Node)) {
+                setIsSettingContainerOpen(false);
+            }
+        };
+
+        document.addEventListener("click", handleClickOutside);
+        return () => document.removeEventListener("click", handleClickOutside);
+    }, [isSettingContainerOpen, setIsSettingContainerOpen]);
+
+    return (<>
+        <div className="fixed w-screen h-screen bg-black opacity-50"></div>
+
+        <div className="relative rounded-b-lg overflow-hidden" ref={containerRef}>
+            <div className="px-4 pt-6 pb-2">
+                <h2 className="mb-4 text-center font-bold text-xl">偏好設定</h2>
+                <div className="grid grid-cols-2 gap-y-2">
+
+                    <label className="flex items-center">券商</label>
+                    <IconicSelect
+                        options={brokeragesIconicSelectOptions}
+                        defaultValue={setting.state.brokerage.id}
+                        onChange={(value) => {
+                            const updBrokerage = brokerageMap.get(value!.toString());
+                            setting.dispatch({ type: "SET_BROKERAGE", payload: updBrokerage });
+                        }}
+                    />
+
+                    <label className="flex items-center">手續費折數</label>
+                    <input
+                        value={setting.state.brokerage.feeDiscountRate}
+                        disabled
+                        className="py-1 rounded text-foreground disabled:p-0 disabled:bg-black"
+                    />
+
+                    <label className="flex items-center">賣價同步買價</label>
+                    <ToggleSwitch
+                        value={setting.state.syncSellPrice}
+                        onChange={(value) => {
+                            setting.dispatch({ type: "SET_SYNC_SELL_PRICE", payload: value });
+                        }}
+                    />
+
+                    <div className="col-span-2 pt-4 flex justify-end gap-2">
+                        <button
+                            onClick={() => localStorage.removeItem("preference")}
+                            className="rounded bg-transparent text-sm underline">
+                            刪除偏好設定
+                        </button>
+                        <button
+                            onClick={() => setting.setLocalstorage(setting.state)}
+                            className="rounded bg-transparent text-sm underline">
+                            儲存偏好設定並在下次使用
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <button
+                onClick={() => setIsSettingContainerOpen((prev) => !prev)}
+                className="w-full">＝</button>
+        </div>
+    </>);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,14 @@
 import { Cog6ToothIcon } from "@heroicons/react/24/solid";
 import Caculator from "./(containers)/Caculator";
 import SettingProvider from "./(states)/SettingState";
-import Setting from "./(containers)/Setting";
+import SettingContainer from "./(containers)/SettingContainer";
 import { useState } from "react";
 
 export default function Home() {
-  const [isSettingOpen, setIsSettingOpen] = useState(false);
+  const [isSettingContainerOpen, setIsSettingContainerOpen] = useState(false);
 
-  function handleSettingClick(){
-    setIsSettingOpen(!isSettingOpen);
+  function handleSettingClick() {
+    setIsSettingContainerOpen((prev) => !prev);
     window.scrollTo({ top: 0, behavior: "smooth" });
   }
 
@@ -17,22 +17,25 @@ export default function Home() {
     <SettingProvider>
       {/* 設定按鈕 */}
       <button
-        onClick={() => setIsSettingOpen(!isSettingOpen)}
-        className="fixed bottom-4 right-4 outline p-2 bg-[#6299d5] rounded-full z-50">
-        <Cog6ToothIcon className={`${isSettingOpen ? '-rotate-[210deg]' : 'rotate-0'} 
-           size-8 transition-all`} />
+        onClick={handleSettingClick}
+        className="fixed bottom-4 right-4 outline p-2 bg-[#6299d5] rounded-full z-50 shadow-md">
+        <Cog6ToothIcon className={`size-8 transition-transform ${isSettingContainerOpen ? 'rotate-[150deg]' : 'rotate-0'}`} />
       </button>
 
+      {/* 偏好設定面板 */}
+      <div className={`fixed top-0 mx-2 max-w-3xl
+          outline outline-1 outline-primary 
+          border border-primary-400 border-t-0 rounded-b-lg bg-black z-40 
+          transform transition-all duration-800 ease-in-out
+          ${isSettingContainerOpen
+          ? "translate-y-0 opacity-100"
+          : "-translate-y-full opacity-0"}`}>
+        <SettingContainer
+          isSettingContainerOpen={isSettingContainerOpen}
+          setIsSettingContainerOpen={setIsSettingContainerOpen} />
+      </div>
+
       <div className="h-screen flex flex-col gap-2 items-center min-h-screen px-4 py-2 pt-10 transition-all">
-
-        {/* 控制面板 */}
-        <div className={`${isSettingOpen ? "max-h-full opacity-100" : "max-h-0 opacity-0"} 
-             px-2 pt-10 pb-2 outline outline-1 outline-primary
-             border border-primary-400 border-t-0 border-b-[1rem] rounded-b-lg w-full bg-black z-40
-             w-full transform -translate-y-[2.5rem] transition-all duration-700`}>
-          <Setting />
-        </div>
-
         {/* 計算器 */}
         <div className="w-full transition-all">
           <Caculator />
