efa7e276b89069eda9e8b69f1ccbebd26ff1b862
c2erichsu <c2erichsu@gmail.com>
2025-02-08 00:09:08 +0800

    Refactor PriceInput component; implement continuous price adjustment on button hold and improve input handling

--- src/app/(components)/PriceInput.tsx
+++ src/app/(components)/PriceInput.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { MinusIcon, PlusIcon } from '@heroicons/react/24/solid';
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction, useRef } from 'react';
 import { getStockInterval } from '../(utilities)/stockInterval';
 import Decimal from 'decimal.js';
 
@@ -10,14 +10,37 @@ interface PriceInputProps {
 }
 
 export default function PriceInput({ value, onChange }: PriceInputProps) {
+    const intervalRef = useRef<NodeJS.Timeout | null>(null);
+    let insideValue: string = value;
 
-    function updatePriceByStep(ctrl: boolean) {
-        const price: number = Number(value);
+    function handlePlus() {
+        if (intervalRef.current) return;
+        intervalRef.current = setInterval(() => {
+            updatePriceByStep(true);
+        }, 100);
+    };
+
+    function handleMinus() {
+        if (intervalRef.current) return;
+        intervalRef.current = setInterval(() => {
+            updatePriceByStep(false);
+        }, 100);
+    };
+
+    function stopInterval() {
+        if (intervalRef.current) {
+            clearInterval(intervalRef.current);
+            intervalRef.current = null;
+        }
+    };
+
+    function updatePriceByStep(isIncrement: boolean) {
+        const price: number = Number(insideValue);
         if (!isNaN(price)) {
             const step = getStockInterval(price);
             let newPrice = new Decimal(price);
-            newPrice = ctrl ? newPrice.add(step) : newPrice.minus(step);
-
+            newPrice = isIncrement ? newPrice.add(step) : newPrice.minus(step);
+            insideValue = newPrice.toString();
             onChange(newPrice.toString());
         } else {
             onChange('0');
@@ -25,11 +48,8 @@ export default function PriceInput({ value, onChange }: PriceInputProps) {
     }
 
     function updatePrice(event: React.ChangeEvent<HTMLInputElement>) {
-        // if (event.target.value === '') {
-        //     return;
-        // }
-        console.log(event.target.value)
         const newPrice = event.target.value;
+        insideValue = newPrice.toString();
         onChange(newPrice);
     }
 
@@ -38,13 +58,23 @@ export default function PriceInput({ value, onChange }: PriceInputProps) {
             <input type="number" inputMode="decimal" value={value} onChange={updatePrice}
                 className='flex-grow'></input>
 
-            <button tabIndex={-1} onClick={() => updatePriceByStep(false)}
-                className='p-2 bg-primary-400 hover:bg-primary-500'>
+            <button tabIndex={-1}
+                onClick={() => updatePriceByStep(false)}
+                onPointerDown={handleMinus}
+                onPointerUp={stopInterval}
+                onPointerLeave={stopInterval}
+                className='p-2 bg-primary-400 select-none
+                    hover:bg-primary-500'>
                 <MinusIcon className='inline-block size-6'></MinusIcon>
             </button>
 
-            <button tabIndex={-1} onClick={() => updatePriceByStep(true)}
-                className='p-2 bg-primary-600 hover:bg-primary-700'>
+            <button tabIndex={-1}
+                onClick={() => updatePriceByStep(true)}
+                onPointerDown={handlePlus}
+                onPointerUp={stopInterval}
+                onPointerLeave={stopInterval}
+                className='p-2 bg-primary-600 select-none
+                    hover:bg-primary-700'>
                 <PlusIcon className='inline-block size-6'></PlusIcon>
             </button>
         </div>
