2abaa6f9aae185cca6ad5d011c45800e39cf6acd
c2erichsu <c2erichsu@gmail.com>
2025-03-26 22:49:57 +0800

    Update logo text to remove space in "TradyFee"

--- src/app/(components)/LogoIcon.tsx
+++ src/app/(components)/LogoIcon.tsx
@@ -2,7 +2,7 @@ export default function LogoIcon() {
     return <svg viewBox="0 0 200 70" xmlns="http://www.w3.org/2000/svg" className="w-full">
         <g transform="translate(0, 40)">
             <text fontFamily="Monoton" fontSize="32" fill="white">
-                <tspan>Trady Fee</tspan>
+                <tspan>TradyFee</tspan>
             </text>
         </g>
 
