8c9a86a1f12434b939072036fcbdf9aec25e8b19
c2erichsu <c2erichsu@gmail.com>
2025-04-11 01:35:52 +0800

    feat: update fetch URL to retrieve stock information based on stockId from request parameters

--- twse-proxy/src/index.ts
+++ twse-proxy/src/index.ts
@@ -1,6 +1,8 @@
 export default {
 	async fetch(request: Request) {
-		const url = 'https://openapi.twse.com.tw/v1/exchangeReport/STOCK_DAY_ALL';
+		const { searchParams } = new URL(request.url);
+		const stockId = searchParams.get('stockId');
+		const url = `https://mis.twse.com.tw/stock/api/getStockInfo.jsp?json=1&delay=0&ex_ch=tse_${stockId}.tw`;
 		const response = await fetch(url);
 		const data = await response.text();
 		return new Response(data, {
